2fa3b5b4265c558f27e96cb703c9a44d1beb7985
FairySunny <fairysunny0214@gmail.com>
2026-05-05 22:44:39 +0800

    refactor

--- src/client/java/com/fairysunny/mc/signpreview/hud/SignPreviewHud.java
+++ src/client/java/com/fairysunny/mc/signpreview/hud/SignPreviewHud.java
@@ -13,8 +13,8 @@ import java.util.function.Consumer;
 
 public class SignPreviewHud {
     private static class SignRenderer extends SignEditScreen implements Consumer<GuiGraphics> {
-        SignRenderer(SignBlockEntity blockEntity, boolean front, boolean filter, Minecraft client) {
-            super(blockEntity, front, filter);
+        SignRenderer(SignBlockEntity blockEntity, boolean front, Minecraft client) {
+            super(blockEntity, front, client.isTextFilteringEnabled());
 
             minecraft = client;
             font = client.font;
@@ -30,8 +30,8 @@ public class SignPreviewHud {
     }
 
     private static class HangingSignRenderer extends HangingSignEditScreen implements Consumer<GuiGraphics> {
-        HangingSignRenderer(SignBlockEntity blockEntity, boolean front, boolean filter, Minecraft client) {
-            super(blockEntity, front, filter);
+        HangingSignRenderer(SignBlockEntity blockEntity, boolean front, Minecraft client) {
+            super(blockEntity, front, client.isTextFilteringEnabled());
 
             minecraft = client;
             font = client.font;
@@ -55,9 +55,9 @@ public class SignPreviewHud {
     public void render(GuiGraphics context, SignBlockEntity blockEntity, boolean front) {
         Consumer<GuiGraphics> renderer;
         if (blockEntity instanceof HangingSignBlockEntity) {
-            renderer = new HangingSignRenderer(blockEntity, front, client.isTextFilteringEnabled(), client);
+            renderer = new HangingSignRenderer(blockEntity, front, client);
         } else {
-            renderer = new SignRenderer(blockEntity, front, client.isTextFilteringEnabled(), client);
+            renderer = new SignRenderer(blockEntity, front, client);
         }
 
         context.flush();
